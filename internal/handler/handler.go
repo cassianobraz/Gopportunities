e4b49bb89bc63cb94868a0ce2330d07b8709516b
cassianobraz <cassanopb@gmail.com>
2026-07-24 00:19:29 -0400

    finish: project with methods crud

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,10 +1,7 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/cassianobraz/Gopportunities/internal/config"
-	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -16,28 +13,4 @@ var (
 func InitializeHandler() {
 	logger = config.GetLogger("handler")
 	db = config.GetSQLite()
-}
-
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
-}
-
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
-}
-
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening",
-	})
-}
-
-func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
 }
---
+++ internal/handler/listOpening.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/cassianobraz/Gopportunities/internal/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningHandler(ctx *gin.Context) {
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
+}
